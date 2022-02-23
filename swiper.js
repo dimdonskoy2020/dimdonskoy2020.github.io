7a3a4364584923bbf545e9c4e2bd57c36dbec646
dimdonskoy2020 <98356702+dimdonskoy2020@users.noreply.github.com>
2022-02-23 16:33:25 +0500

    добавил swiper.css и swiper.js

---
+++ swiper.js
@@ -0,0 +1,29 @@
+new Swiper('.swiper', {
+  //Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  loop: true,
+  //Навигацияъ
+  // буллеты, текущее положение, прогрессбар
+  pagination: {
+    el: '.swiper-pagination',
+    // Буллеты
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 16
+    },
+    // when window width is >= 480px
+   
+    // when window width is >= 640px
+    768: {
+       slidesPerView: 1.2,
+      spaceBetween: 16
+    }
+  }
+});
